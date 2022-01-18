50e93773f1d14e2d80daf7fbebcb5c8a9cf79bbd
dnspsv <dns_spsv@hotmail.com>
2022-01-18 20:40:19 -0300

    alterações necessárias

--- quasar.conf.js
+++ quasar.conf.js
@@ -141,8 +141,8 @@ module.exports = configure(function (ctx) {
       },
 
       manifest: {
-        name: 'Quasar App',
-        short_name: 'Quasar App',
+        name: 'Quasar Tempo',
+        short_name: 'Quasar Tempo',
         description: 'A Quasar Framework app',
         display: 'standalone',
         orientation: 'portrait',
