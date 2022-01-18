73cf97c791481fbfd50362f145ad74a6ea5dfb55
dnspsv <dns_spsv@hotmail.com>
2022-01-18 20:53:55 -0300

    nova modificações

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,14 @@
-
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Index.vue') }
-    ]
+    component: () => import('layouts/loginLayout.vue'),
+    children: [{ path: '', component: () => import('pages/login.vue') }]
   },
-
+  {
+    path: '/auth',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/Index.vue') }]
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
