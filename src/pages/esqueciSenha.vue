f33738ed7d54c70e3f30174d6b843f104affb870
dnspsv <dns_spsv@hotmail.com>
2022-01-18 21:54:13 -0300

    up de hoje

---
+++ src/pages/esqueciSenha.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-page class="flex flex-center bg-primary">
+    <q-card style="padding: 20px">
+
+      <q-card-section class="q-pt-none full-width">
+        <p class="text-h5">Esqueci minha senha</p>
+        <div class="text-caption">Informe seu e-mail para recuperar a senha.</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none full-width">
+        <q-input filled v-model="email" label="Email" />
+      </q-card-section>
+
+      <q-card-actions class="full-width q-mt=sm">
+        <q-btn
+          class="full-width q-mx-sm"
+          color="primary"
+          label="Redefinir senha"
+          style="wigth: 100%"
+          @click="$router.push({ name: 'login' })"
+        />
+      </q-card-actions>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      email: ''
+    }
+  },
+  methods: {}
+}
+</script>
--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,13 +1,65 @@
 <template>
-  <q-page class="flex flex-center">
-    <p class="text-h2">Login</p>
+  <q-page class="flex flex-center bg-primary">
+    <q-card class="">
+      <img src="../assets/logo.png" ration="1" />
+
+      <q-card-section class="q-pt-none">
+
+        <q-input filled v-model="login.email" label="Email" />
+
+        <q-input
+          class="q-mt-sm"
+          v-model="login.password"
+          label="Password"
+          filled
+          :type="login.isPwd ? 'password' : 'text'"
+        >
+          <template v-slot:append>
+            <q-icon
+              :name="login.isPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="login.isPwd = !login.isPwd"
+            />
+          </template>
+        </q-input>
+
+      </q-card-section>
+      <q-card-actions class="full-width">
+        <q-btn
+          class="full-width q-mx-sm"
+          color="primary"
+          label="Login"
+          style="wigth: 100%"
+          @click="$router.push({ name: 'home' })"
+        />
+      </q-card-actions>
+      <q-card-actions class="full-width">
+        <q-btn
+          flat
+          no-caps
+          class="full-width q-mx-sm"
+          color="primary"
+          label="Esqueci minha sanha"
+          style="wigth: 100%"
+          @click="$router.push({ name:'esqueciSenha'})"
+        />
+      </q-card-actions>
+    </q-card>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'PageIndex'
-})
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      login: {
+        email: '',
+        password: '',
+        isPwd: true
+      }
+    }
+  },
+  methods: {}
+}
 </script>
